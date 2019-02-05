8e3bf9410ae701f44556fc5ec7c72b9fa888e216
SuperWeebTanks <trilleo12@live.dk>
2019-02-05 13:24:48 +0100

    Added Divider method as well as Accumulator property.

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Calculator
 {
@@ -10,22 +13,46 @@ namespace Calculator
     {
         public double Add(double a, double b)
         {
+            Accumulator = a + b;
             return a + b;
         }
 
         public double Subtract(double a, double b)
         {
+            Accumulator = a - b;
             return a - b;
         }
 
         public double Multiply(double a, double b)
         {
+            Accumulator = a * b; 
             return a * b;
         }
 
         public double Power(double x, double exp)
         {
+            Accumulator = Math.Pow(x, exp); 
             return Math.Pow(x, exp);
         }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            double value = dividend / divisor;
+            Accumulator = value; 
+            return value;
+        }
+
+        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0; 
+        }
+
     }
 }

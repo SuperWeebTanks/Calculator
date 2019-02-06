7bdfab3c592d044b961f94d5ca3a9994b6672f61
MH <mathhan01@gmail.com>
2019-02-06 11:07:55 +0100

    Added test case of power method

--- Calculator.Test.Unit/Calculator.Test.Unit.cs
+++ Calculator.Test.Unit/Calculator.Test.Unit.cs
@@ -94,7 +94,7 @@ namespace Calculator.Test.Unit
         [Test]
         public void Divide_Divide32point5with22point32_Result1point456()
         {
-            Assert.That(uut.Divide(35.5, 22,32), Is.EqualTo(1.456).Within(0.1));
+            Assert.That(uut.Divide(35.5, 22.32), Is.EqualTo(1.59).Within(0.1));
         }
         
     }
